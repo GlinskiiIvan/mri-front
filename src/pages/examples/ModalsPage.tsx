b5e9124db9401760fe1a0a66c61ea53a9268f711
Glinsky Ivan <glinsky_ivan@mail.ru>
2026-04-06 12:06:14 +0500

    рефактор модалки, добавил useModal для удобства использования

--- src/pages/examples/ModalsPage.tsx
+++ src/pages/examples/ModalsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Block, Button, InfoList, Modal, Page, Stack } from '../../ui/copmonents';
 import { patientInfoList } from '../../utils';
+import { useModal } from '../../ui/copmonents/Modal/useModal';
 
 const pageProps = {
     title: 'Демонстрация вариантов модального окна' ,
@@ -8,7 +9,7 @@ const pageProps = {
 }
 
 const ModalsPage: React.FC = () => {
-    const [modal, setVisibleModal] = React.useState(false);
+    const testModal = useModal('test-modal');
 
     return (
         <Page 
@@ -17,12 +18,12 @@ const ModalsPage: React.FC = () => {
             decorativeIcon='INFO' >
             <Block>
                 <Stack direction='row' gap='md' justify='flex-start' align='center'>
-                    <Button variant={'primary'} onClick={() => setVisibleModal(true)}>Open modal</Button>
+                    <Button variant={'primary'} onClick={() => testModal.open()}>Open modal</Button>
                 </Stack>
             </Block>
 
             <Modal  
-                visible={modal} setVisible={setVisibleModal} 
+                options={testModal} 
                 title='Test modal' footer={
                     <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '16px'}}>
                         <Button variant='primary' icon='ADD'>Обследование</Button>
